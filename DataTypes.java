2991d12e65410d14051bf890450e7a08d31db517
Motassim <motassimkhan4@gmail.com>
2024-03-01 11:52:49 +0530

    data types added

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // data types in java
+        // byte stores values from -128 to 128
+        byte a = 100;
+        // short stores values from -32768 to 32767
+        short b = 200;
+        // int stores values from -2147483648 to 2147483647
+        int c = 500;
+        // long stores values from -9223372036854775808 to 9223372036854775807
+        long d = 10000000l;
+        // double and float store decimal values
+        double e = 5.5;
+        float f = 10.5f;
+        // char stores single character 
+        char g = 'A';
+        // boolean stores true or false values
+        boolean h = true;
+    }
+}
