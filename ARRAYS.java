68b4b0747af2503f82f66ec642b9d08a0b0a7089
Motassim <motassimkhan4@gmail.com>
2024-03-05 16:50:32 +0530

    Arrays

--- ARRAYS.java
+++ ARRAYS.java
@@ -1,7 +1,30 @@
+package com.company;
+
 public class ARRAYS {
     public static void main(String[] args) {
-        // to create an array we write: data_type [] arr_name = new data/-type[size]; unlike C we specify the square braces after the data type in java
-        int [] int_array = {1,2,3,4,5,6,7,8,9,10};
-        System.out.println(int_array);
+        /* Classroom of 500 students - You have to store marks of these 500 students
+        You have 2 options:
+        1. Create 500 variables
+        2. Use Arrays (recommended)
+        */
+        // There are three main ways to create an array in Java
+        // 1. Declaration and memory allocation
+        // int [] marks = new int[5];
+
+        // 2. Declaration and then memory allocation
+        // int [] marks;
+        // marks = new int[5];
+        // Initialization
+        // marks[0] = 100;
+        // marks[1] = 60;
+        // marks[2] = 70;
+        // marks[3] = 90;
+        // marks[4] = 86;
+
+        // 3. Declaration, memory allocation and initialization together
+        int [] marks = {98, 45, 79, 99, 80};
+
+        // marks[5] = 96; - throws an error
+        System.out.println(marks[4]);
     }
 }
